05988b773920f03f321c5a86f38c8ea33fcb3711
ArunKumar000777 <arunashok1020@gmail.com>
2022-12-09 16:49:40 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 import CategoryList from "./components/CategoryList";
+import { Routes, Route } from "react-router-dom";
+import ShopComponent from "./routes/shop/ShopComponent";
+
 function App() {
     return (
         <div className="App">
-            <CategoryList/>
+            <CategoryList />
+            <ShopComponent/>
         </div>
     );
 }
